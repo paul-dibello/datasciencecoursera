102b315cd01e08ea6984077bb3cf3f4a0ed38c98
Paul DiBello <paul.dibello@yale.edu>
2016-01-03 21:40:38 -0500

    Update to rankhospital

--- ProgrammingAssignment3/rankhospital.R
+++ ProgrammingAssignment3/rankhospital.R
@@ -19,10 +19,10 @@ outComesLookup <- rbind( outComesLookup, data.frame("heart attack" = 11,
 hospCol <- 2
 
   if(!state %in% states){
-    stop("invalid State")
+    stop("invalid state")
   }
   if(!outcome %in% outcomes){
-    stop("Invalid Outcome")
+    stop("invalid outcome")
   }
   outComeCol <- outComesLookup[[outcome]]
   if(outcome == "heart attack"){
@@ -31,7 +31,34 @@ hospCol <- 2
   if(outcome == "heart failure"){
     outComeCol = 17
   }
-
   
-reorderedDF <- outcomeDF[ order(outcomeDF[,outComeCol], outcomeDF[,hospCol], na.last = TRUE),]
+  statesubsetDF <- subset(outcomeDF, State == state)
+
+  if(num == "best")
+  {
+    hospDF <- (statesubsetDF[which.min(statesubsetDF[[outComeCol]]),])
+    output = hospDF[["Hospital.Name"]]
+  }
+  else if(num == "worst"){
+    hospDF <- (statesubsetDF[which.max(statesubsetDF[[outComeCol]]),])
+    output = hospDF[["Hospital.Name"]]
+  }
+  else {
+    reorderedDF <- statesubsetDF[ order(as.integer(statesubsetDF[,outComeCol]), statesubsetDF[,hospCol], na.last = TRUE),]
+    integerList <- as.integer(reorderedDF[[outComeCol]])
+    ##integerList2 <- reorderedDF[["Hospital.Name"]]
+    ##print(integerList2)
+    ##print(integerList)
+    outcomeLength <- length(na.omit(integerList))
+    ##print(outcomeLength)
+    number <- as.integer(num)
+    if (number > outcomeLength){
+      output = "NA"
+    } else {
+      hospDF <- reorderedDF[number,]
+      output = hospDF[["Hospital.Name"]]
+    }
+    
+  }
+output
 }
